928134d5267299a260c9a5fb3381d92d64148ad4
Azmul <azmulg@gmail.com>
2026-05-16 13:21:18 +0600

    fix: fixed the product

--- src/globals/Footer.ts
+++ src/globals/Footer.ts
@@ -2,12 +2,16 @@ import type { GlobalConfig } from 'payload'
 
 import { adminOnly } from '@/access/adminOnly'
 import { link } from '@/fields/link'
+import { revalidateFooter } from '@/globals/hooks/revalidateFooter'
 
 export const Footer: GlobalConfig = {
   slug: 'footer',
   access: {
     read: () => true,
     update: adminOnly,
+  },
+  hooks: {
+    afterChange: [revalidateFooter],
   },
   fields: [
     {
--- src/globals/Header.ts
+++ src/globals/Header.ts
@@ -2,12 +2,16 @@ import type { GlobalConfig } from 'payload'
 
 import { adminOnly } from '@/access/adminOnly'
 import { link } from '@/fields/link'
+import { revalidateHeader } from '@/globals/hooks/revalidateHeader'
 
 export const Header: GlobalConfig = {
   slug: 'header',
   access: {
     read: () => true,
     update: adminOnly,
+  },
+  hooks: {
+    afterChange: [revalidateHeader],
   },
   fields: [
     {
